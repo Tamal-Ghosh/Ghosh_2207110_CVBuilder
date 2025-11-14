d6f8633ef73ba8ac378ffdd575b263f9ae274826
Tamal Ghosh <tamalghosha07@gmail.com>
2025-11-14 12:05:54 +0600

    form page built

--- src/main/java/com/example/ghosh_2207110_cvbuilder/FormController.java
+++ src/main/java/com/example/ghosh_2207110_cvbuilder/FormController.java
@@ -1,4 +1,43 @@
 package com.example.ghosh_2207110_cvbuilder;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class FormController {
+
+    @FXML
+    private Button btCVBuild;
+
+    @FXML
+    private TextArea inputAdd;
+
+    @FXML
+    private TextArea inputEducation;
+
+    @FXML
+    private TextField inputEmail;
+
+    @FXML
+    private TextField inputName;
+
+    @FXML
+    private TextArea inputNumber;
+
+    @FXML
+    private TextArea inputProject;
+
+    @FXML
+    private TextArea inputSkills;
+
+    @FXML
+    private TextArea inputWork;
+
+    @FXML
+    void OnbtnClkBuildCv(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/com/example/ghosh_2207110_cvbuilder/HelloController.java
+++ src/main/java/com/example/ghosh_2207110_cvbuilder/HelloController.java
@@ -2,8 +2,13 @@ package com.example.ghosh_2207110_cvbuilder;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
 
@@ -14,8 +19,12 @@ public class HelloController {
     private Label welcomeLabel;
 
     @FXML
-    void OnClickCreateCv(ActionEvent event) {
-
+    void OnClickCreateCv(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("form.fxml"));
+        Scene scene=new Scene(fxmlLoader.load());
+        Stage stage=(Stage) (welcomeLabel.getScene().getWindow());
+        stage.setScene(scene);
+        stage.show();
 
     }
 
